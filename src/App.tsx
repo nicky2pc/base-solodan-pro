82831fde887b82e4c60fd83d81250d6c886e0c95
nick <nick@nicks-MacBook-Air.local>
2026-04-24 10:00:39 +0500

    chore: remove deprecated sdk.actions.ready() from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import Game from './components/Game/Game';
 import { Providers } from './providers/Provider';
-import { sdk } from '@farcaster/miniapp-sdk';
 
 function App() {
-  useEffect(() => {
-    sdk.actions.ready();
-  }, []);
-
   return (
     <div className="App">
       <Providers>
